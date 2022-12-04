ddc224d5e4268221c653df4205ccf07887e3550e
IldarAbdulin <abdulinildar1@gmail.com>
2022-12-04 21:02:20 +0600

    did 4 tasks

--- script.js
+++ script.js
@@ -23,7 +23,6 @@
 // console.log(merge(arr2))
 // console.log(merge(arr3))
 
-
 //2
 // const prices1 = [7, 1, 5, 3, 6, 4];
 // const prices2 = [7, 1, 6, 4, 3];
@@ -41,7 +40,6 @@
 // }
 // console.log(maxProfit(prices1))
 // console.log(maxProfit(prices2))
-
 
 //3
 // const fizzBuzz = (num) => {
@@ -62,7 +60,6 @@
 // }
 // console.log(fizzBuzz(50))
 
-
 //4
 // const button = document.querySelector('#btn')
 // const palindrome = (p) => {
@@ -78,7 +75,6 @@
 // }
 // button.addEventListener('click', palindrome)
 
-
 //5
 // let input1 = [1, 8, 6, 2, 5, 4, 8, 3, 7];
 // let input2 = [1, 1];
@@ -90,7 +86,7 @@
 //     let maxArea = 0;
 //     let left = 0;
 //     let right = height.length - 1;
-    
+
 //     while(left < right) {
 //         let currentVolume = Math.min(height[left], height[right]) * (right - left);
 //         maxArea = Math.max(maxArea, currentVolume);
@@ -108,7 +104,6 @@
 // console.log(maxArea(input3))
 // console.log(maxArea(input4))
 
-
 //6
 // const arr = [1, 5, 2, 10, 3, 8]
 
@@ -125,9 +120,7 @@
 // const first = arr.slice(0, 1);
 // console.log(first)
 
-
-
-//7 
+//7
 // const arr1 = [1, 1, 2, 2, 3, 3]
 // const arr2 = [0, 0, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3];
 
@@ -146,7 +139,6 @@
 // console.log('----------')
 // console.log(removeDuplicates(arr2))
 // console.log(arr2)
-
 
 //8
 // const findVowels = (str) => {
@@ -168,7 +160,6 @@
 // console.log(findVowels('aaaooo')) //6
 // console.log(findVowels('why')) //0
 
-
 //9
 // const classNames = ['header', 'menu', 'menu-item', 'menu-item', 'menu-item', 'footer', 'menu' , 'link', 'link', 'link' , 'link'];
 // for(let i = 0; i < classNames.length; i++) {
@@ -179,7 +170,6 @@
 // }
 
 // console.log(classNames);
-
 
 //10
 // const numbers = (number) => {
@@ -194,12 +184,9 @@
 // }
 // console.log(numbers([20, 5, 10, -100, 1, 9, 5, 500]))
 
-
-
 //11
 // let test = true
 // test ? console.log('Неверно') : console.log('Верно');
-
 
 //12
 // let number;
@@ -211,7 +198,6 @@
 // data ? number = 3 : number = 5;
 // console.log(number);
 
-
 //13
 // let numbers = [-5, 10, 55, -1, 22, -4, 36, -45]
 // let minus = []
@@ -222,12 +208,10 @@
 // let minus = numbers.filter(a => a < 0)
 // console.log(minus)
 
-
 //14
 // let filterFalses = [false, 45, 'str', null, 0, 1, undefined, 0];
 // let typeWasFalse = filterFalses.filter(falsy => falsy == false);
 // console.log(typeWasFalse);
-
 
 //таблица умножения
 // for (i = 1; i <= 10; i++){
@@ -238,7 +222,6 @@
 //     console.log(" ");
 // }
 
-
 //15
 // const numbersInReverse = (num) => {
 //     let reverseNums = [...num.toString()].map(Number).sort((a, b) => b - a);
@@ -246,23 +229,19 @@
 // }
 // console.log(numbersInReverse(12358))
 
-
-
 //16
 // const yearOfBirth = prompt('Год вашего рождения');
 // const thisYear = prompt('Текущий год');
 // console.log(thisYear - yearOfBirth);
 
-
 //17
 // const arr = [1, 2, 3, 4];
 // const arrKv = []
 // for(let i = 1; i <= arr.length; i++) {
-//     const arrSort = (i ** 2);   
+//     const arrSort = (i ** 2);
 //     arrKv.push(arrSort)
 // }
 // console.log(arrKv)
-
 
 //18
 // const firstNum = +prompt('Введите первое число');
@@ -280,10 +259,59 @@
 //     }
 // }
 
-
 //19
 // const data = [1, 2, 3];
 // function myFunc(arr) {
 //     return arr = arr.reverse();
 // }
 // console.log(myFunc(data));
+
+//20
+// const digitalRoot = (n) => {
+//     const arr = [...n.toString()].map(Number)
+//     const result = arr.reduce((a ,b) => a + b , 0)
+
+//     if(result >= 10) {
+//         let secondResult = [...result.toString()].map(Number).reduce((a, b) => a + b ,0)
+//         return secondResult
+//     }
+
+//     return result
+// }
+// console.log(digitalRoot(942));
+
+//21
+// let count = 0;
+// const helloWorld = () => {
+//     if(count === 5) return alert('done');
+//     count++
+//     helloWorld(alert('Hello world'))
+// }
+// helloWorld();
+
+// function pow(x, y) {
+//     if(y === 0) return 1;
+//     return x * pow(x, y - 1);
+// }
+// console.log(pow(5,3));
+
+//22
+// function sumOfDigits(number) {
+//   return number < 10 ? 1 : 1 + sumOfDigits(Math.floor(number / 10));
+// }
+// console.log(sumOfDigits(1234));
+
+
+//23
+// function urlGenerator(domain) {
+//     return function(url) {
+//         return `https://${url}.${domain}`
+//     }
+// }
+// const comUrl = urlGenerator('com');
+// const ruUrl = urlGenerator('ru')
+
+// console.log(comUrl('google'));
+// console.log(comUrl('youtube'));
+
+// console.log(ruUrl('yandex'));
